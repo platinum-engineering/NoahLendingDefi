2ad8b6d7eb7b1d570d5b799a13d6a963ef415013
pillar.guru <2622610@gmail.com>
2021-02-12 22:32:15 +0300

    SIMPLE MIGRATIONS

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,13 @@
-const Migrations = artifacts.require("Migrations");
+const Controller = artifacts.require("NLTController")
+const Token = artifacts.require("NoahLendToken")
+const PriceOracle = artifacts.require("NoahPriceOracleAnchoredView")
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
-};
+module.exports = async function (deployer, network) {
+  try {
+    await deployer.deploy(Token)
+    await deployer.deploy(Controller)
+    await deployer.deploy(PriceOracle, Token.address)
+  } catch (err) {
+    console.error(err)
+  }
+}
